18f612fff81524138aef5edbef9d185c38784d57
hlchoi <hl8469@gmail.com>
2021-08-31 17:10:33 +0900

    add unittest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def       helloworld(a):
-    print(f"Hello World! {a}")#안녕하세요
+def helloworld(a):
+    msg = f"Hello World! {a}"
+    print(msg)  # 안녕하세요
+    return msg
+
+
 if __name__ == "__main__":
     helloworld("HwaLang")
-
-
